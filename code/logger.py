c27b6aaae2c6e4e165c0a8691ce639d6a140b0fe
Yaqing Bie <bieyaqing@gmail.com>
2019-09-11 18:34:44 +0800

    update

--- code/logger.py
+++ code/logger.py
@@ -34,7 +34,7 @@ class Logger(object):
         if self.logger.hasHandlers():
             for hdl in self.logger.handlers:
                 self.logger.removeHandler(hdl)
-        handler = RotatingFileHandler(LOG_FILE_PATH, maxBytes=50 * 1024 * 1024, backupCount=100, encoding="UTF-8")
+        handler = RotatingFileHandler(logger.LOG_FILE_PATH, maxBytes=50 * 1024 * 1024, backupCount=100, encoding="UTF-8")
         handler.setFormatter(logging.Formatter(FORMAT))
         self.logger.addHandler(handler)
         self.server_ip = gethostbyname(gethostname())
