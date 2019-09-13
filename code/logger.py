143f4508d1f232eea54fa79d09312b7f66574a3b
yaqing.bie <bieyaqing@gmail.com>
2019-09-13 10:46:15 +0800

    update

--- code/logger.py
+++ code/logger.py
@@ -1,10 +1,8 @@
+import json
+import logging
 
-import logging, json
-
-from configparser import ConfigParser
 from datetime import datetime
 from logging.handlers import RotatingFileHandler
-from os.path import expanduser
 from queue import Queue
 from socket import gethostbyname
 from socket import gethostname
@@ -17,35 +15,42 @@ home = expanduser("~")
 
 config.read(f"{home}/.elk_setup/config.ini")
 
+
 class Logger(object):
 
     NOW = datetime.now()
     NOW_DATE_STR = NOW.strftime("%Y-%m-%d")
-    CONF_NAME = config["Logging"].get("LOGGINGFILE", "INFO")
-    LOG_FILE_PATH = f"{home}/logs/{NOW_DATE_STR}-{CONF_NAME}"
     FORMAT = "%(levelname)s %(asctime)-15s %(_server_ip)s %(_port)s %(_filename)s:%(_function)s:%(_line)d %(message)s"
-    # logging.basicConfig(filename=LOG_FILE_PATH, filemode="a", format=FORMAT)
-    CONF_LEVEL = config["Logging"].get("LOGGINGLEVEL", "INFO")
-    LEVEL = logging._nameToLevel[CONF_LEVEL]
+    
+    LOGGINGFILE = config["Logging"].get("LOGGINGFILE", "default.log")
+    LOGGINGLEVEL = config["Logging"].get("LOGGINGLEVEL", "INFO")
 
-    def __init__(self, logger_name=__name__, port="0000"):
+    LOG_PATH = f"{home}/logs/{NOW_DATE_STR}-{LOGGINGFILE}"
+    LEVEL = logging._nameToLevel[LOGGINGLEVEL]
+
+    def __init__(self, logger_name=__name__, log_path=Logger.LOG_PATH, port="0000"):
         self.logger = logging.getLogger(name=logger_name)
         self.logger.setLevel(Logger.LEVEL)
         if self.logger.hasHandlers():
             for hdl in self.logger.handlers:
                 self.logger.removeHandler(hdl)
-        handler = RotatingFileHandler(Logger.LOG_FILE_PATH, maxBytes=50 * 1024 * 1024, backupCount=100, encoding="UTF-8")
+        handler = RotatingFileHandler(log_path, maxBytes=50 * 1024 * 1024, backupCount=100, encoding="UTF-8")
         handler.setFormatter(logging.Formatter(Logger.FORMAT))
         self.logger.addHandler(handler)
         self.server_ip = gethostbyname(gethostname())
         self.port = port
         self.log_stream_queue = Queue()
         self.log_stop_event = Event()
-        t = Thread(target=self.logging_thread_method, args=(self.log_stream_queue, self.log_stop_event,))
-        t.start()
+        self.t = Thread(target=self.logging_thread_method, args=(self.log_stream_queue, self.log_stop_event,))
+        self.t.setDaemon(True)
+        self.t.start()
 
-    def logging_thread_method(self, log_stream_queue, stop_event):
-        while not stop_event.is_set():
+    def stop(self):
+        self.log_stop_event.set()
+        self.t.join()
+
+    def logging_thread_method(self, log_stream_queue, log_stop_event):
+        while not log_stop_event.is_set():
             if not log_stream_queue.empty():
                 raw_str = log_stream_queue.get()
                 raw_obj = json.loads(raw_str)
@@ -88,7 +93,7 @@ class Logger(object):
     def info(self, msg, *args, port=None):
         _filename, _line, _function, _stack = self.logger.findCaller(stack_info=True)
         self.log_stream_queue.put(self._prepare_queue_obj("info", msg, *args, extra=self._prepare_extra(_filename, _line, _function, port)))
-        
+
     def warning(self, msg, *args, port=None):
         _filename, _line, _function, _stack = self.logger.findCaller(stack_info=True)
         self.log_stream_queue.put(self._prepare_queue_obj("warning", msg, *args, extra=self._prepare_extra(_filename, _line, _function, port)))
@@ -104,29 +109,3 @@ class Logger(object):
     def critical(self, msg, *args, port=None):
         _filename, _line, _function, _stack = self.logger.findCaller(stack_info=True)
         self.log_stream_queue.put(self._prepare_queue_obj("critical", msg, *args, extra=self._prepare_extra(_filename, _line, _function, port)))
-
-class SharedLogger(object):
-
-    logger = Logger("SHARED")
-
-    @staticmethod
-    def info(msg, *args, port=None):
-        SharedLogger.logger.info(msg, *args, port=port)
-
-    @staticmethod
-    def warning(msg, *args, port=None):
-        SharedLogger.logger.warning(msg, *args, port=port)
-
-    @staticmethod
-    def error(msg, *args, port=None):
-        SharedLogger.logger.error(msg, *args, port=port)
-
-    @staticmethod
-    def debug(msg, *args, port=None):
-        SharedLogger.logger.debug(msg, *args, port=port)
-
-    @staticmethod
-    def critical(msg, *args, port=None):
-        SharedLogger.logger.critical(msg, *args, port=port)
-
-        
